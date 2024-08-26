8b3a6fe135cc16b0a4c32d0e68efd9bd908f5bbf
sajjad-no <game1540849805@gmail.com>
2024-08-25 22:41:50 -0700

    hello again

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <h1 className="flex  justify-center font-bold text-gray-700 hover:text-black p-10 text-5xl transition duration-500">GOLD KOROSH</h1>
-     <Navbar>hhhhh</Navbar>
+     <Navbar>hello</Navbar>
      
     </>
   )
